04b0d528f53a22b91620a45be9d6a1619babbf1d
LesFerch <79026235+LesFerch@users.noreply.github.com>
2024-09-15 18:37:52 -0400

    Update ClearDefenderHistoryEveryRestart.cmd

--- ClearDefenderHistoryEveryRestart.cmd
+++ ClearDefenderHistoryEveryRestart.cmd
@@ -1,3 +1,3 @@
-:If not already elevated, right-click this batch file and select "Run as Administrator"
+::If not already elevated, right-click this batch file and select "Run as Administrator"
 
 schtasks /create /f /sc onStart /ru "NT AUTHORITY\SYSTEM" /tn DWDH /tr "cmd.exe /c rd /s /q 'C:\ProgramData\Microsoft\Windows Defender\Scans\History\Service'"
