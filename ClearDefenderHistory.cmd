e5338fdb6b3f9f0c6d337f3d184058db7d8972af
LesFerch <79026235+LesFerch@users.noreply.github.com>
2024-09-20 12:01:55 -0400

    Add files via upload

--- ClearDefenderHistory.cmd
+++ ClearDefenderHistory.cmd
@@ -4,15 +4,25 @@ powershell -nop "if (-not ([Security.Principal.WindowsPrincipal][Security.Princi
 goto :eof
 #>
 
-$folder = 'C:\ProgramData\Microsoft\Windows Defender\Scans\History\Service'
-$file = "$folder\Detections.log"
+$ClearAV = $true
+$ClearCFA = $true
+$RemoveTask = $true
 
-if (Test-Path $file) {
-  schtasks /create /f /sc onStart /ru 'NT AUTHORITY\SYSTEM' /tn DWDH /tr "cmd.exe /c rd /s /q \`"$folder\`" & schtasks /delete /f /tn DWDH"
+# ClearAV: Clear history of detected viruses and malware
+# ClearCFA: Clear history of Controlled Folder Access blocks
+# RemoveTask: Remove task after running
 
-  $choice = (Read-Host "`nA restart is required to clear the Protection history. Enter y to restart now").ToLower()
-  if ($choice -eq "y") { Restart-Computer }
-}
-else {
-  Read-Host "No Protection history (Detections.log) found. Press Enter to close"
-}
+# If $RemoveTask is set to $false, then you would run this script just once to create a permanent task that clears the protection history every restart. Do not set this option to $false unless you really never want to see the protection history.
+
+$Scans = 'C:\ProgramData\Microsoft\Windows Defender\Scans'
+$Service = "$Scans\History\Service"
+$db = "$Scans\mpenginedb.db*"
+
+if ($ClearAV)    { $C1 = "rd /s /q \`"$Service\`" & " }
+if ($ClearCFA)   { $C2 = "del \`"$db\`" & " }
+if ($RemoveTask) { $C3 = "schtasks /delete /f /tn DWDH" }
+
+schtasks /create /f /sc onStart /ru 'NT AUTHORITY\SYSTEM' /tn DWDH /tr "cmd.exe /c $C1$C2$C3"
+
+$choice = (Read-Host "`nA restart is required to clear the Protection history. Enter y to restart now").ToLower()
+if ($choice -eq "y") { Restart-Computer }
